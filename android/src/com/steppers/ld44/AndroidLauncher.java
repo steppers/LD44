388a1003ae896cb76d23e70673b4878a4053717e
steppers <oliver.steppers@gmail.com>
2019-04-26 23:10:35 +0100

    Add LibGDX template

---
+++ android/src/com/steppers/ld44/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.steppers.ld44;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.steppers.ld44.LD44Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD44Game(), config);
+	}
+}
---
+++ desktop/src/com/steppers/ld44/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.steppers.ld44.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.steppers.ld44.LD44Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD44Game(), config);
+	}
+}
