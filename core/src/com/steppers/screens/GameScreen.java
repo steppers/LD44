eefe96c39fc2bf49f7e38b27b62da97f90370b31
C S <charlies.1998@googlemail.com>
2019-04-27 17:54:18 +0100

    Added Background

--- core/src/com/steppers/screens/GameScreen.java
+++ core/src/com/steppers/screens/GameScreen.java
@@ -1,5 +1,9 @@
 package com.steppers.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.steppers.ld44.Renderer;
 import com.steppers.ui.UIButton;
@@ -12,6 +16,10 @@ public class GameScreen extends UIScreen {
 
     UIButton backButton;
 
+    Texture background;
+
+    SpriteBatch batch;
+
     public GameScreen() {
         shapeRenderer = Renderer.Get().GetShapeRenderer();
 
@@ -20,12 +28,21 @@ public class GameScreen extends UIScreen {
             UIManager.Get().transitionToScreen("main_menu", 0.3f);
         });
         registerElement(backButton);
+
+        Pixmap px=new Pixmap(Gdx.files.internal("StoneWallTileableSmall.png"));
+        background=new Texture(px);
+        background.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+
+        batch = new SpriteBatch();
     }
 
     @Override
     public void render(float opacity) {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        batch.begin();
+        batch.draw(background,0,0, 0, 0, Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
         backButton.render(opacity);
+        batch.end();
         shapeRenderer.end();
     }
 }
