8a7bf1aa619829d3f970a279263dd5d7785a54c8
steppers <oliver.steppers@gmail.com>
2019-04-29 00:51:48 +0100

    Add Icon support for UIButtons

--- core/src/com/steppers/ui/UIButton.java
+++ core/src/com/steppers/ui/UIButton.java
@@ -1,6 +1,8 @@
 package com.steppers.ui;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.steppers.ld44.Renderer;
 
@@ -11,24 +13,37 @@ public class UIButton extends UIElement {
     }
 
     ShapeRenderer shapeRenderer;
+    SpriteBatch spriteBatch;
     Color color;
     Color baseColor;
     ButtonHandler handler;
 
     boolean mouseDown;
+    Texture icon;
+
+    public UIButton(float x, float y, float width, float height, Texture icon) {
+        this(x, y, width, height, new Color(0.2f, 0.2f, 0.2f, 1.0f), icon);
+    }
 
     public UIButton(float x, float y, float width, float height) {
-        this(x, y, width, height, new Color(0.2f, 0.2f, 0.2f, 1.0f));
+        this(x, y, width, height, new Color(0.2f, 0.2f, 0.2f, 1.0f), null);
     }
 
     public UIButton(float x, float y, float width, float height, Color color) {
+        this(x, y, width, height, color, null);
+    }
+
+    public UIButton(float x, float y, float width, float height, Color color, Texture icon) {
         super(x, y, width, height);
 
         shapeRenderer = Renderer.Get().GetShapeRenderer();
+        spriteBatch = Renderer.Get().GetSpriteBatch();
+
         baseColor = color;
         this.color = new Color(baseColor);
         mouseDown = false;
         handler = null;
+        this.icon = icon;
     }
 
     public void setHandler(ButtonHandler handler) {
@@ -36,11 +51,13 @@ public class UIButton extends UIElement {
     }
 
     public boolean handleMouseEvent(float x, float y, MouseEvent event) {
+        boolean handled = false;
         if(isMouseOver(x, y)) {
             switch (event) {
                 case MOUSE_DOWN:
                     color.set(baseColor.r*2, baseColor.g*2, baseColor.b*2, 1.0f);
                     mouseDown = true;
+                    handled = true;
                     break;
                 case MOUSE_UP:
                     color.set(baseColor);
@@ -49,19 +66,21 @@ public class UIButton extends UIElement {
                         handler.onActivate();
                     }
                     mouseDown = false;
+                    handled = true;
                     break;
-                case MOUSE_MOVED:
                 case MOUSE_DRAGGED:
+                    handled = true;
+                case MOUSE_MOVED:
                     color.set(baseColor.r*1.5f, baseColor.g*1.5f, baseColor.b*1.5f, 1.0f);
                     break;
             }
         } else {
             color.set(baseColor);
             mouseDown = false;
-            return false;
+            handled = false;
         }
 
-        return true;
+        return handled;
     }
 
     public void update(float dt) {}
@@ -71,5 +90,15 @@ public class UIButton extends UIElement {
         shapeRenderer.rect( bounds.x, bounds.y, bounds.width, bounds.height);
         shapeRenderer.setColor(color.r*0.5f, color.g*0.5f, color.b*0.5f, opacity);
         shapeRenderer.rect( bounds.x+2, bounds.y+2, bounds.width-4, bounds.height-4);
+    }
+
+    public void renderText(float opacity) {
+        if(icon != null) {
+            float iconSize = Math.min(bounds.width, bounds.height) * 0.8f;
+            float iconOffsetX = (bounds.width - iconSize)/2;
+            float iconOffsetY = (bounds.height - iconSize)/2;
+            spriteBatch.setColor(1, 1, 1, opacity);
+            spriteBatch.draw(icon, bounds.x + iconOffsetX, bounds.y + iconOffsetY, iconSize, iconSize);
+        }
     }
 }
